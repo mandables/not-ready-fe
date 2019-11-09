94da1cff31be78e934097d61dd9a5b670bea43b3
amaan19 <amaan19@hotmail.com>
2019-11-09 22:12:05 +0000

    Added stats

---
+++ src/Stats.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class Stats extends Component {
+	render() {
+		const { stats } = this.props;
+		return (
+			<div className="stats-container">
+				<div className="stat" id="stat-one">
+					<p id="title">Top commenter</p>
+					<br />
+					<p>Abujee</p>
+				</div>
+				<div className="stat" id="stat-two">
+					<p id="title">Most prolific day</p>
+					<br />
+					<p>02/11/2019</p>
+				</div>
+				<div className="stat" id="stat-three">
+					<p id="title">Inquirers on my side</p>
+					<br />
+					<p>21%</p>
+				</div>
+			</div>
+		);
+	}
+}
